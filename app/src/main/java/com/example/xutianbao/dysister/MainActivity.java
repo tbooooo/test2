e40c2efa0b8e6b7c7c6eda236d5a245b6261c8bc
tbooooo <10446748@qq.com>
2018-07-05 16:29:49 +0800

    branch test

--- app/src/main/java/com/example/xutianbao/dysister/MainActivity.java
+++ app/src/main/java/com/example/xutianbao/dysister/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.xutianbao.dysister;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.app.Activity;
+
+//让Activity方法实现OnClickListener接口
+public class MainActivity extends Activity implements OnClickListener{
+    private Button btnshow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnshow = (Button) findViewById(R.id.btn_show);
+        //直接写个this
+        btnshow.setOnClickListener(this);
+    }
+    //重写接口中的抽象方法
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "点击了按钮", Toast.LENGTH_SHORT).show();
     }
 }
