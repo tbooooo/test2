0f156cb0b9bd87495366886f8bea1d9216679fc2
tbooooo <10446748@qq.com>
2018-07-06 14:29:09 +0800

    76commit

--- app/src/main/java/com/example/xutianbao/dysister/MainActivity.java
+++ app/src/main/java/com/example/xutianbao/dysister/MainActivity.java
@@ -1,31 +1,64 @@
 package com.example.xutianbao.dysister;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.app.Activity;
+import android.view.View.OnClickListener;
 
-//让Activity方法实现OnClickListener接口
-public class MainActivity extends Activity implements OnClickListener{
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
     private Button btnshow;
+    private ImageView imgshow;
+    private int curPos=0;
+    private ArrayList<String> urls;
+    private PictureLoader loader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loader = new PictureLoader();
+        initUI();
+        initData();
+    }
+    private void initData(){
+        urls = new ArrayList<>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+    private void initUI(){
+        btnshow = findViewById(R.id.btn_show);
+        imgshow = findViewById(R.id.img_show);
+        btnshow.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    switch(view.getId()){
+                        case R.id.btn_show:
+                            if(curPos>9){
+                                curPos=0;
+                            }
+                            loader.load(imgshow,urls.get(curPos));
+                            curPos++;
+                            break;
+                    }
 
-        btnshow = (Button) findViewById(R.id.btn_show);
-        //直接写个this
-        btnshow.setOnClickListener(this);
+            }
+        });
     }
-    //重写接口中的抽象方法
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(getApplicationContext(), "点击了按钮", Toast.LENGTH_SHORT).show();
-    }
+
 }
